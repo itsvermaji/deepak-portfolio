738b9783a01558f09b6cd4ba78ac781e76e7d84f
Deepak Verma <deepakverma.pb34@gmail.com>
2025-02-28 01:36:42 +0530

    github pages configuration added

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -8,11 +8,12 @@ import Contact from "../pages/contact/ContactComponent";
 import Projects from "../pages/projects/Projects";
 import { settings } from "../portfolio.js";
 import Error404 from "../pages/errors/error404/Error";
+import { HashRouter } from "react-router-dom/cjs/react-router-dom.min.js";
 
 export default class Main extends Component {
   render() {
     return (
-      <BrowserRouter basename="/">
+      <HashRouter basename="/">
         <Switch>
           <Route
             path="/"
@@ -63,7 +64,7 @@ export default class Main extends Component {
             render={(props) => <Error404 {...props} theme={this.props.theme} />}
           />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
